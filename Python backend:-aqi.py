83aa9e2ef5c359cb293f18d5d26fb5843712001a
PSR-128 <162555042+PSR-128@users.noreply.github.com>
2024-03-25 11:03:33 +0530

    Create Python backend:-aqi.py
    this is the backend framework for all the html web pages. The file name is aqi.py

---
+++ Python backend:-aqi.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('aqi.html')
+
+@app.route('/get_aqi')
+def get_aqi():
+    city = request.args.get('city')  # Changed 'requests' to 'request'
+    response = requests.get(f'https://api.waqi.info/feed/{city}/?token=a5ce4f19eae698a8cd0088d064a2876a218d614c')
+    data = response.json()
+    if 'data' in data and 'aqi' in data['data']:
+        aqi = data['data']['aqi']
+        return jsonify({'aqi': aqi})
+    else:
+        return jsonify({'error': 'AQI data not available for the specified city'})
+
+@app.route('/home.html')
+def home_page():
+    return render_template('home.html')  # Rendering home.html
+
+@app.route('/quiz.html')
+def quiz_page():
+    return render_template('quiz.html')  # Renders quiz.html
+
+@app.route('/aqi.html')
+def aqi_page():
+    return render_template('aqi.html')  # Renders quiz.html
+
+if __name__ == '__main__':
+    app.run(debug=True)
