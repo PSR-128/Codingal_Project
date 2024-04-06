ceec72e97d3702495a07e1d907c9a61d5ff6483e
PSR-128 <162555042+PSR-128@users.noreply.github.com>
2024-04-06 11:23:53 +0530

    Update Python backend:-aqi.py
    now the home page comes first

--- Python backend:-aqi.py
+++ Python backend:-aqi.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('aqi.html')
+    return render_template('home.html')
 
 @app.route('/get_aqi')
 def get_aqi():
@@ -18,21 +18,38 @@ def get_aqi():
     else:
         return jsonify({'error': 'AQI data not available for the specified city'})
 
+
+@app.route('/news_content')
+def news_content():
+    # Make a request to the News API endpoint
+    response = requests.get('https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=94b4958687e44ec381ee8088a34bf7d3')
+    if response.ok:
+        # Extract news content from the response
+        news_content = response.text
+        return render_template('news.html', news_content=news_content)
+    else:
+        return 'Failed to fetch news content'
+
+# Routes for other pages
 @app.route('/home.html')
 def home_page():
-    return render_template('home.html')  # Rendering home.html
+    return render_template('home.html')
 
 @app.route('/quiz.html')
 def quiz_page():
-    return render_template('quiz.html')  # Renders quiz.html
+    return render_template('quiz.html')
 
 @app.route('/aqi.html')
 def aqi_page():
-    return render_template('aqi.html')  # Renders quiz.html
+    return render_template('aqi.html')
 
 @app.route('/clean_energy.html')
 def clean_energy_page():
-    return render_template('clean_energy.html')  # Renders quiz.html
+    return render_template('clean_energy.html')
+
+@app.route('/news.html')
+def news_page():
+    return render_template('news.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
